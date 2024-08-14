d02b3fea7ea790ed7eee3602d90b756a53e3b3b8
James Anderson <james.anderson98@outlook.com>
2024-08-14 11:36:29 +0100

    Add basic Gameboard tests

--- src/battleships.test.js
+++ src/battleships.test.js
@@ -1,4 +1,6 @@
 import { Ship } from "./Ship";
+import { Gameboard } from "./Gameboard";
+
 // SHIP TESTS
 test('Ship class - length test', () => {
     const testShip = new Ship(3);
@@ -27,3 +29,29 @@ test('Ship class - ship has been sunk', () => {
     testShip.hit();
     expect(testShip.isSunk()).toBe(true);
 });
+
+
+// GAMEBOARD TESTS
+test('Gameboard class - place ship', () => {
+    const board = new Gameboard();
+    board.place([1, 1], [1, 3]);
+    board.place([3, 0], [0, 0]);
+    expect(board.shipLocations).toEqual([ [1, 1], [1, 2], [1, 3], [3, 0], [2, 0], [1, 0], [0, 0] ])
+});
+
+test('Gameboard class - reject diagonal', () => {
+    const board = new Gameboard();
+    expect(() => board.place([0, 0], [2, 2])).toThrow(Error);
+});
+
+test('Gameboard class - reject too short or too long', () => {
+    const board = new Gameboard();
+    expect(() => board.place([0, 0], [0, 6])).toThrow(Error);
+    expect(() => board.place([0, 0], [1, 0])).toThrow(Error);
+});
+
+// test('Gameboard class - receive attack', () => {
+//     const board = new Gameboard();
+//     board.place([0, 0], [4, 0]);
+//     board.receiveAttack([])
+// })
