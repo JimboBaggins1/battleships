9bdb011934cbdc057abeec716a0e679fde6c07b4
James Anderson <james.anderson98@outlook.com>
2024-08-23 11:04:26 +0100

    Add a Player class
    (cherry picked from commit dc07214a9b5f5e7b5dcdca9d84336dbf9a647d55)

---
+++ src/Player.js
@@ -0,0 +1,7 @@
+import { Gameboard } from "./Gameboard";   
+export class Player {
+    constructor(isComputer = false) {
+        this.isComputer = isComputer;
+        this.gameboard = new Gameboard;
+    }
+}
